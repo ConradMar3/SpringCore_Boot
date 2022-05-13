1927966b7fa0c525559c4b79621d8da7bdffcdae
ConradMar3 <96224782+ConradMar3@users.noreply.github.com>
2022-05-13 15:20:12 +0200

    2022/05/13 (pm)
    Part2 - Integrating Spring

---
+++ Workspaces/Part2/src/main/java/tacocloud/Part2IntegratedSpringApplication.java
@@ -0,0 +1,13 @@
+package tacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Part2IntegratedSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Part2IntegratedSpringApplication.class, args);
+	}
+
+}
---
+++ Workspaces/Part2/src/test/java/tacocloud/Part2IntegratedSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Part2IntegratedSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
